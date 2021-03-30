3f7ce8d417ea5b003d6271744812148b5bbe9f55
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-03-29 19:29:45 -0600

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -1,5 +1,4 @@
-start cmd.exe /k "node filewriter.js"
-start cmd.exe /k "python mainx.py"  
-start cmd.exe /k "cd C:\FlyJus\dev\ardupilot\build\sitl\bin && arduplane.exe -Mxplane -s1 --uartA tcp:0" 
-start cmd.exe /k "cd C:\Program Files (x86)\Mission Planner && missionplanner.exe" 
-
+start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\x-plane_calc && node fileWriter.js"
+start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\x-plane_calc && python mainx.py" 
+start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\ardupilot\build\sitl\bin && arduplane.exe -Mxplane -s1 --uartA tcp:0" 
+start cmd.exe /k "cd C:\Program Files (x86)\Mission Planner && MissionPlanner.exe" 
