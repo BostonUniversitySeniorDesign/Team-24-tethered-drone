3970d7c786b12b9077ecd100aabb71796497f00e
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-03-31 09:20:22 -0600

    Update mainx.py

--- mainx.py
+++ mainx.py
@@ -1,5 +1,7 @@
 from calculate import *
 from plot import *
+import sys
+import time
 def main(x_vec, y_vec,line1, F, V, A, cll, cdd):
 
 	xlist = calc(F, V, A, cll, cdd)
@@ -10,12 +12,13 @@ def main(x_vec, y_vec,line1, F, V, A, cll, cdd):
 	plist = [line1, xlist]
 
 	print("Power is: ", xlist[0])
-	print("Force is: ", xlist[1])
-	print("Velocity is: ", xlist[2])
+	#print("Force is: ", xlist[1])
+	#print("Velocity is: ", xlist[2])
 
 	return plist
 
 if __name__ == "__main__":
+	start = time.time()
 	size = 5
 	x_vec = np.linspace(0,1,size+1)[0:-1]
 	#print(x_vec)
@@ -25,8 +28,9 @@ if __name__ == "__main__":
 	A = 1.5 #m^2
 	cll = 0
 	cdd = 0
+	avg_power = []
 
-	default = input("Do you want to run with the default settings? (y/n)")
+	default = input("Do you want to run with the default settings? (y/n) \n")
 
 	if default == 'y':
 		plist = main(x_vec, y_vec, line1, 0, 0, A, cll, cdd)
@@ -41,6 +45,45 @@ if __name__ == "__main__":
 		plist = main(x_vec, y_vec, line1, 0, 0, A, cll, cdd)
 
 	while True:
-		y_vec[-1] = plist[1][0]
-		plist = main(x_vec, y_vec, plist[0], plist[1][1], plist[1][2], A, cll, cdd)
-		y_vec = np.append(y_vec[1:],0.0)
+		try:
+			y_vec[-1] = plist[1][0]
+			plist = main(x_vec, y_vec, plist[0], plist[1][1], plist[1][2], A, cll, cdd)
+			y_vec = np.append(y_vec[1:],0.0)
+			avg_power.append(plist[1][0])
+
+			if len(avg_power) == (sys.maxsize - 10):
+				print("\n")
+				print("Reached maximum number of items in list")
+				print("Please restart")
+				break
+				
+		except KeyboardInterrupt:
+			elapsed = time.time() - start
+			totalpow = sum(avg_power)
+			avgpow = totalpow/len(avg_power)
+			totalenergy = totalpow*elapsed
+
+			print("\n")
+			print("======================================")
+			print("Total Power generated so far: " + str(totalpow))
+			print("Average Power generated so far: " + str(avgpow))
+			print("Total Energy generated so far: " + str(totalenergy))
+			print("======================================")
+			print("\n")
+
+			kbi = input("Would you like to continue? (y/n)\n")
+
+			if kbi == 'y':
+				print("Continuing with program...")
+				print("\n")
+				continue
+			elif kbi == 'n':
+				print("End of program")
+				break
+			else:
+				print("Invalid selection")
+				print("Continuing with program...")
+				print("\n")
+				print
+				continue
+
