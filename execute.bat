af7485ccd9692ee7e0f85690e972f1064cf35e71
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-04-08 15:50:52 -0600

    Update execute.bat
    Can't use Mission Planner anymore. Meaning no more ardupilot

--- execute.bat
+++ execute.bat
@@ -1,4 +1,2 @@
 start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\x-plane_calc && node fileWriter.js"
 start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\x-plane_calc && python mainx.py" 
-start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\ardupilot\build\sitl\bin && arduplane.exe -Mxplane -s1 --uartA tcp:0" 
-start cmd.exe /k "cd C:\Program Files (x86)\Mission Planner && MissionPlanner.exe" 
