9362c5e8e20eaf20688d632fd44e3577c44d307d
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-04-09 01:25:41 -0600

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -1,2 +1,3 @@
 start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\x-plane_calc && node fileWriter.js"
 start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\x-plane_calc && python mainx.py" 
+start cmd.exe /k "cd C:\cygwin_64\home\JadenCho\x-plane_calc && python justplots.py" 
