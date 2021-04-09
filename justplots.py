2fa09b8b1fbe72f0666076b03d4209618b4205e2
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-04-09 01:25:22 -0600

    Update justplots.py
    This graphs the wind velocities

--- justplots.py
+++ justplots.py
@@ -1,24 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import re
+from calculate import *
+
 # use ggplot style for more sophisticated visuals
 plt.style.use('ggplot')
 
-def live_plotter(x_vec,y1_data,line1,identifier='',pause_time=0.65):
+def live_plotter_multi(x_vec,y1_data,y2_data, y3_data, y4_data, line1, line2, line3, line4, label, name, identifier='',pause_time=0.65):
     if line1==[]:
         # this is the call to matplotlib that allows dynamic plotting
         plt.ion()
-        fig = plt.figure()
+        fig = plt.figure(2)
         ax = fig.add_subplot(111)
         # create a variable for the line so we can later update it
-        line1, = ax.plot(x_vec,y1_data,'-o',alpha=0.8)        
+        line1, = ax.plot(x_vec,y1_data,'-o',alpha=0.8)  
+        line2, = ax.plot(x_vec,y2_data,'-o',alpha=0.8)
+        line3, = ax.plot(x_vec,y3_data,'-o',alpha=0.8)
+        line4, = ax.plot(x_vec,y4_data,'-o',alpha=0.8)      
         #update plot label/title
-        plt.ylabel('Y Label')
-        plt.title('Title: Beta Angle'.format(identifier))
+        plt.legend([line1, line2, line3, line4],['Wind in X', 'Wind in Y', 'Wind in Z', 'Absolute Wind Speed'])
+        plt.ylabel(label)
+        plt.title(name.format(identifier))
         plt.show()
     
     # after the figure, axis, and line are created, we only need to update the y-data
     line1.set_ydata(y1_data)
+    line2.set_ydata(y2_data)
+    line3.set_ydata(y3_data)
+    line4.set_ydata(y4_data)
     # adjust limits if new data goes beyond bounds
     if np.min(y1_data)<=line1.axes.get_ylim()[0] or np.max(y1_data)>=line1.axes.get_ylim()[1]:
         plt.ylim([np.min(y1_data)-np.std(y1_data),np.max(y1_data)+np.std(y1_data)])
@@ -26,129 +34,61 @@ def live_plotter(x_vec,y1_data,line1,identifier='',pause_time=0.65):
     plt.pause(pause_time)
     
     # return line so we can update it again in the next iteration
-    return line1
+    return [line1, line2, line3, line4]
 
 if __name__ == "__main__":
     size = 5
     x_vec = np.linspace(0,1,size+1)[0:-1]
     #print(x_vec)
-    y_vec = np.zeros(len(x_vec))
+    y_vec1 = np.zeros(len(x_vec))
+    y_vec2 = np.zeros(len(x_vec))
+    y_vec3 = np.zeros(len(x_vec))
+    y_vec4 = np.zeros(len(x_vec))
     #print(y_vec)
     line1 = []
+    line2 = []
+    line3 = []
+    line4 = []
 
-    while True:
-        #rand_val = np.random.randn(1)
-        file = open("C:\\FlyJus\\dev\\ardupilot\\build\\sitl\\bin\\buffer.bin", "r")
-        s = file.readline()
-        print(type(s))
-        print(s)
-        f = float(s)
-        print(type(f))
-        print(f)
-        y_vec[-1] = f
-        line1 = live_plotter(x_vec,y_vec,line1)
-        y_vec = np.append(y_vec[1:],0.0)
+    wind_on = input("Do you want to see wind data? (y/n) \n")
 
-        myfile = open('C:\\FlyJus\\dev\\ardupilot\\build\\sitl\\bin\\bufferJava.bin', "r")
-        a = 'sim/flightmodel/forces/fnrml_aero'
-        b = 'sim/flightmodel/forces/faxil_aero'
-        c = 'sim/airfoils/afl_cl'
-        d = 'sim/airfoils/afl_cd'
-        e = 'sim/cockpit/autopilot/vertical_velocity'
-        f = 'sim/flightmodel/misc/cl_overall'
-        g = 'sim/flightmodel/misc/cd_overall'
-        h = 'sim/flightmodel/position/local_x'
-        ii = 'sim/flightmodel/position/local_y'
-        jj = 'sim/flightmodel/position/local_z'
-        k = 'sim/flightmodel/position/true_phi'
-        l = 'sim/flightmodel/position/true_psi'
-        m = 'sim/flightmodel/position/local_vx'
-        n = 'sim/flightmodel/position/local_vy'
-        o = 'sim/flightmodel/position/local_vz'
-        p = 'sim/flightmodel/position/alpha'
-        q = 'sim/flightmodel/position/groundspeed'
-        r = 'sim/flightmodel/position/true_airspeed'
+    if wind_on == 'y':
 
-        #pre-allocate vectors for bufferJava (the data) and where datarefs stored
-        dataarray = [] #for datarefs
-        #betaarray = [] #for beta values
-        fnrmal_match = []
-        faxil_aero_match = []
-        afl_cl_match = []
-        afl_cd_match = []
-        vertical_velocity_match = []
-        cl_overall_match = []
-        cd_overall_match = []
-        local_x_match = []
-        local_y_match = []
-        local_z_match = []
-        true_phi_match = []
-        true_psi_match = []
-        local_vx_match = []
-        local_vy_match = []
-        local_vz_match = []
-        alpha_match = []
-        groundspeed_match = []
-        true_airspeed_match = []
+        xlist = calc(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
 
-        #temporarily store data in 'data' and we will append it to pre-allocated
-        #dataarray
-        data = myfile.readlines()
-        for i in data:
-            dataarray.append(i)
-            #lets check which strings match the datarefs and append them to
-            #corresponding pre-allocated vector
-            if a == dataarray[0][0:len(a)]:
-                fnrmal_match.append(dataarray)
-                print(fnrmal_match)
-            if b == dataarray[0][0:len(b)]:
-                faxil_aero_match.append(dataarray)
-                print(faxil_aero_match)
-            if c == dataarray[0][0:len(c)]:
-                afl_cl_match.append(dataarray)
-                print(afl_cl_match)
-            if d == dataarray[0][0:len(d)]:
-                afl_cd_match.append(dataarray)
-                print(afl_cd_match)
-            if e == dataarray[0][0:len(e)]:
-                vertical_velocity_match.append(dataarray)
-                print(vertical_velocity_match)
-            if f == dataarray[0][0:len(f)]:
-                cl_overall_match.append(dataarray)
-                print(cl_overall_match)
-            if g == dataarray[0][0:len(g)]:
-                cd_overall_match.append(dataarray)
-                print(cd_overall_match)
-            if h == dataarray[0][0:len(h)]:
-                local_x_match.append(dataarray)
-                print(local_x_match)
-            if ii == dataarray[0][0:len(ii)]:
-                local_y_match.append(dataarray)
-                print(local_y_match)
-            if jj == dataarray[0][0:len(jj)]:
-                local_z_match.append(dataarray)
-                print(local_z_match)
-            if k == dataarray[0][0:len(k)]:
-                true_phi_match.append(dataarray)
-                print(true_phi_match)
-            if l == dataarray[0][0:len(l)]:
-                true_psi_match.append(dataarray)
-                print(true_psi_match)
-            if m == dataarray[0][0:len(m)]:
-                local_vx_match.append(dataarray)
-                print(local_vx_match)
-            if n == dataarray[0][0:len(n)]:
-                local_vy_match.append(dataarray)
-                print(local_vy_match)
-            if o == dataarray[0][0:len(o)]:
-                local_vz_match.append(dataarray)
-                print(local_vz_match)
-            if p == dataarray[0][0:len(p)]:
-                alpha_match.append(dataarray)
-                print(alpha_match)
-            if q == dataarray[0][0:len(q)]:
-                groundspeed_match.append(dataarray)
-                print(groundspeed_match)
-            if r == dataarray[0][0:len(r)]:
-                true_airspeed_match.append(dataarray)
-                print(true_airspeed_match)
+        while True:
+            try:
+                
+                xlist = calc(0, 0, 0, 0, 0, 0, 0, 0, xlist[6], xlist[7], xlist[8], xlist[9])
+
+                y_vec1[-1] = xlist[6]
+                y_vec2[-1] = xlist[7]
+                y_vec3[-1] = xlist[8]
+                y_vec4[-1] = xlist[9]
+
+                [line1, line2, line3, line4] = live_plotter_multi(x_vec, y_vec1, y_vec2, y_vec3, y_vec4, line1, line2, line3, line4, 'Wind Speed (Knots)', 'Wind Velocity')
+       
+                y_vec1 = np.append(y_vec1[1:],0.0)
+                y_vec2 = np.append(y_vec2[1:],0.0)
+                y_vec3 = np.append(y_vec3[1:],0.0)
+                y_vec4 = np.append(y_vec4[1:],0.0)
+
+            except KeyboardInterrupt:
+                back_on = input("You have paused. Continue? (y/n) \n")
+
+                if back_on == 'y':
+                    print("Continuing with program...\n")
+                    continue
+
+                elif back_on == 'n':
+                    print("Halting Program...\n")
+                    break
+
+                else:
+                    print("Invalid Selection\n")
+                    print("Continuing with Program\n")
+                    continue
+
+    else:
+        print("Goodbye\n")
+
