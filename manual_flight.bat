3d6eb025df249a508b4f96d5e255338a22fe4326
henrybojanowski <henryboj@bu.edu>
2021-04-09 20:09:23 -0400

    add new .bat file that will run the appropriate programs for manual flight

---
+++ manual_flight.bat
@@ -0,0 +1,5 @@
+start cmd.exe /k "node filewriter.js"
+start cmd.exe /k "python mainx.py"  
+start cmd.exe /k "python justplots.py" 
+rem start cmd.exe /k "cd C:\Program Files (x86)\Mission Planner && MissionPlanner.exe" 
+
