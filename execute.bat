91867e0d8e08b26111221d5fc426ed9d352a6ed7
JadenCho <34437519+JadenCho@users.noreply.github.com>
2021-04-11 23:19:44 -0600

    Update execute.bat

--- execute.bat
+++ execute.bat
@@ -1,4 +1,5 @@
 start cmd.exe /k "cd PATH\TO\FILE\LOCATION && node fileWriter.js"
 start cmd.exe /k "cd PATH\TO\FILE\LOCATION && python mainx.py" 
 start cmd.exe /k "cd PATH\TO\FILE\LOCATION && python justplots.py" 
+start cmd.exe /k "cd PATH\TO\FILE\LOCATION && arduplane.exe -Mxplane -s1 --uartA tcp:0"
 start cmd.exe /k "cd PATH\TO\FILE\LOCATION && MissionPlanner.exe"
