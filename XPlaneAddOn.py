88c20bafef325674e83966de09d95732444b2fb2
JadenCho <choj@bu.edu>
2021-04-26 22:09:01 -0600

    Comments

--- XPlaneAddOn.py
+++ XPlaneAddOn.py
@@ -4,37 +4,40 @@ import sys
 import time
 def main(x_vector, y_vector, line1, F, V, A, cll, cdd, s1, s2, s3):
 
+	#Function from CalculatePower
 	returned_array_calculate_drone_power = calculate_drone_power(F, V, A, cll, cdd, s1, s2, s3, 0, 0, 0, 0)
+	#Function from DynamicPlotting
 	line1 = live_plotter(x_vector, y_vector, line1, 'Power (KiloWatts)', 'Drone Power Generation')
 
-	
-
+	#Assign outputs from functions to an array
 	plist = [line1, returned_array_calculate_drone_power]
 
+	#Formatted printing to the console
 	input1=returned_array_calculate_drone_power[0]
 	formatted_string_1 = "{:.0f}".format(input1)
 	float_value_power=float(formatted_string_1)
 	print("Power is: ", float_value_power, " kW")
-	
 
 	return plist
 
 if __name__ == "__main__":
-	start = time.time()
-	size = 5
+	start = time.time() #Setting a start time to keep track of elapsed time
+	size = 5 #Setting inital vector sizes
 	x_vector = np.linspace(0,1,size+1)[0:-1]
 	#print(x_vector)
 	y_vector = np.zeros(len(x_vector))
 	#print(y_vector)
-	line1 = []
+	line1 = [] #Empty array for Dynamic Plotting
 	
+	#Default values of the drone
 	A = 1.5 #m^2
 	cll = 0
 	cdd = 0
-	avg_power = []
+	avg_power = [] #Empty array to store power values
 
 	default = input("Do you want to run with the default settings? (y/n) \n")
 
+	#User interface
 	if default == 'y':
 		plist = main(x_vector, y_vector, line1, 0, 0, A, cll, cdd, 0, 0, 0)
 	elif default == 'n':
@@ -49,20 +52,24 @@ if __name__ == "__main__":
 
 	while True:
 		try:
+			#Infinitely looping the main function to run the calculations and plot the power
 			y_vector[-1] = plist[1][0]
 
 			plist = main(x_vector, y_vector, plist[0], plist[1][1], plist[1][2], A, cll, cdd, plist[1][3], plist[1][4], plist[1][5])
 
 			y_vector = np.append(y_vector[1:],0.0)
 
+			#Append the resulting power to the array
 			avg_power.append(plist[1][0])
 
+			#Error checking in case max number of array elements is reached
 			if len(avg_power) == (sys.maxsize - 10):
 				print("\n")
 				print("Reached maximum number of items in list")
 				print("Please restart simulation")
 				break
 				
+		#Allows user to pause and quit from program
 		except KeyboardInterrupt:
 			elapsed = time.time() - start
 			totalpow = sum(avg_power)
@@ -70,7 +77,6 @@ if __name__ == "__main__":
 
 			formatted_string_total_pow= "{:.2f}".format(totalpow_kW)
 			float_value_total_pow=float(formatted_string_total_pow)
-
 
 			avgpow = totalpow/len(avg_power)
 			totalenergy = totalpow*elapsed
@@ -84,8 +90,6 @@ if __name__ == "__main__":
 			formatted_string_total_energy = "{:.2f}".format(totalenergy)
 			float_value_total_energy = float(formatted_string_total_energy)
 
-
-
 			print("\n")
 			print("======================================")
 			print("Total Power generated so far: " + str(float_value_total_pow) + " kW")
@@ -96,6 +100,7 @@ if __name__ == "__main__":
 
 			kbi = input("Would you like to continue? (y/n)\n")
 
+			#User interface for quitting or continuing
 			if kbi == 'y':
 				avg_power.clear()#clear list for avg power
 				#clear list for total power
